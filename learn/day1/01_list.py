5c09b3e3511995a8e7611a123781bdaf8b73cf49
Sudeep Dhakal <38913917+sudeep360@users.noreply.github.com>
2022-06-04 21:03:43 +1000

    Added a faulty code to demostrate list

---
+++ learn/day1/01_list.py
@@ -0,0 +1,7 @@
+#storing user entered fruit items as a list 
+
+fruits=[]
+x=int(input("Enter a number of fruits that you want to add:"))
+for i in range(x):
+    fruits.item(i,input(f"Add fruit {i}:"))
+    
